afadd5140ae5cac6610832796d4abc7091e0e53a
Ahmad-Sawalqeh <53290785+Ahmad-Sawalqeh@users.noreply.github.com>
2020-08-07 19:36:13 +0300

    deployment

---
+++ build/precache-manifest.e1ce2cbe90a0167ff57528a8113040b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568dbc6b3371ca7d6e14a53f5b0300b4",
+    "url": "/arrange-game/index.html"
+  },
+  {
+    "revision": "fbde8f346f746ba385bc",
+    "url": "/arrange-game/static/css/main.c8737574.chunk.css"
+  },
+  {
+    "revision": "e3c339dc5ab5a615b692",
+    "url": "/arrange-game/static/js/2.6eccdff7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/arrange-game/static/js/2.6eccdff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbde8f346f746ba385bc",
+    "url": "/arrange-game/static/js/main.07a3c526.chunk.js"
+  },
+  {
+    "revision": "6fd434a4bc9eb9d6ea85",
+    "url": "/arrange-game/static/js/runtime-main.88f9594e.js"
+  }
+]);
